9121c36d938e6bdc507ceb14759b6ce4c69cfab6
Saurabh Tajane <tajsaurabh123@gmail.com>
2024-11-06 16:11:45 +0530

    added new constants

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,1 +1,15 @@
 export const DB_NAME: string = "panel";
+
+type IUserRole = {
+    USER: string,
+    ADMIN: string
+}
+
+export const userRole: IUserRole = {
+    USER: "USER",
+    ADMIN: "ADMIN"
+}
+
+export const availableUserRole: string[] = Object.values(userRole)
+
+export const USER_TEMPORARY_TOKEN_EXPIRY: number = 10 * 60 * 1000 // 10 min
