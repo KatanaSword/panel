2e1b1267596c3e56d993143dccc5b697c90665a7
Saurabh Tajane <tajsaurabh123@gmail.com>
2024-12-01 13:03:32 +0530

    update type

---
+++ backend/src/utils/ApiError.ts
@@ -0,0 +1,25 @@
+class ApiError {
+  data: null;
+  success: boolean;
+  constructor(
+    public statusCode: number,
+    public message: string = "Something went wrong",
+    public errors: [] = [],
+    public stack: any
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.message = message;
+    this.errors = errors;
+    this.data = null;
+    this.success = false;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
