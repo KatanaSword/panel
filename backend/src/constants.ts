8f8725830a1b74028c8f29b3417fd7d9448fefa8
Saurabh Tajane <tajsaurabh123@gmail.com>
2024-12-03 12:49:46 +0530

    added options cookie constants

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,15 +1,15 @@
 export const DB_NAME: string = "panel";
 
-type IUserRole = {
-    USER: string,
-    ADMIN: string
-}
+export const userRole: { USER: string; ADMIN: string } = {
+  USER: "USER",
+  ADMIN: "ADMIN",
+};
 
-export const userRole: IUserRole = {
-    USER: "USER",
-    ADMIN: "ADMIN"
-}
+export const availableUserRole: string[] = Object.values(userRole);
 
-export const availableUserRole: string[] = Object.values(userRole)
+export const USER_TEMPORARY_TOKEN_EXPIRY: number = 10 * 60 * 1000; // 10 min
 
-export const USER_TEMPORARY_TOKEN_EXPIRY: number = 10 * 60 * 1000 // 10 min
+export const options: { httpOnly: boolean; secret: boolean } = {
+  httpOnly: true,
+  secret: true,
+};
