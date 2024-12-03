df0cebdfb4938791fd1874f7ef1ecd2e0ff2fc41
Saurabh Tajane <tajsaurabh123@gmail.com>
2024-12-03 12:53:24 +0530

    added signInUser and generateAccessRefreshToken controller

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -4,6 +4,27 @@ import { ApiResponse } from "../utils/ApiResponse.ts";
 import { asyncHandler } from "../utils/asyncHandler.ts";
 import { Request, Response } from "express";
 import { IUser } from "../type.ts";
+import { options } from "../constants.ts";
+
+const generateAccessRefreshToken = async (
+  userId: string
+): Promise<{ accessToken: string; refreshToken: string }> => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken: string = user?.generateAccessToken();
+    const refreshToken: string = user?.generateRefreshToken();
+
+    await user?.save({ validateBeforeSave: false });
+    user!.refreshToken = refreshToken;
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "An error occurred while generating tokens. Please try again later."
+    );
+  }
+};
 
 const userRegister = asyncHandler(async (req: Request, res: Response) => {
   const { username, email, phoneNumber, password, role } = req.body as IUser;
@@ -53,4 +74,48 @@ const userRegister = asyncHandler(async (req: Request, res: Response) => {
     );
 });
 
-export { userRegister };
+const signInUser = asyncHandler(async (req: Request, res: Response) => {
+  const { email, phoneNumber, password } = req.body as IUser;
+  if (!email.trim() || !phoneNumber.trim() || !password) {
+    throw new ApiError(400, "Field is empty");
+  }
+
+  const user = await User.findOne({
+    $or: [
+      {
+        email: email.trim().toLowerCase(),
+        phoneNumber: phoneNumber.trim().toLowerCase(),
+      },
+    ],
+  });
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  const isPasswordCorrect: boolean = await user.isPasswordValid(password);
+  if (!isPasswordCorrect) {
+    throw new ApiError(400, "User password invalid");
+  }
+
+  const { refreshToken, accessToken } = await generateAccessRefreshToken(
+    user?._id
+  );
+
+  const signedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  res
+    .status(200)
+    .cookie("refreshToken", refreshToken, options)
+    .cookie("accessToken", accessToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        { user: signedInUser, refreshToken, accessToken },
+        "User successfully sign in"
+      )
+    );
+});
+
+export { userRegister, signInUser };
