bc582f6ad86459f17327cf88eda7c2e592d2bca8
Saurabh Tajane <tajsaurabh123@gmail.com>
2024-12-06 14:52:11 +0530

    more router added

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -1,12 +1,24 @@
 import { Router } from "express";
-import { signInUser, userRegister } from "../controllers/user.controller.ts";
+import {
+  accountDetailUpdate,
+  getCurrentUser,
+  refreshAccessToken,
+  signInUser,
+  signOutUser,
+  userRegister,
+} from "../controllers/user.controller.ts";
+import { verifyJWT } from "../middlewares/auth.middleware.ts";
 
 const router = Router();
 
 // Unsecure routes
 router.route("/register").post(userRegister);
 router.route("/signin").post(signInUser);
+router.route("/refresh_token").post(refreshAccessToken);
 
 // Secure routes
+router.route("/signout").post(verifyJWT, signOutUser);
+router.route("/current_user").get(verifyJWT, getCurrentUser);
+router.route("/account_update").patch(verifyJWT, accountDetailUpdate);
 
 export default router;
