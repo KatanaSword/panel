bb4eb8759afb2d44e10ec7862bde63bf67656f70
Saurabh Tajane <tajsaurabh123@gmail.com>
2024-12-07 13:26:31 +0530

    added verifyPermission for check user is ADMIN or not

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -1,3 +1,4 @@
+import { userRole } from "../constants.ts";
 import { User } from "../models/user.models.ts";
 import { ApiError } from "../utils/ApiError.ts";
 import { asyncHandler } from "../utils/asyncHandler.ts";
@@ -6,14 +7,14 @@ import jwt from "jsonwebtoken";
 
 const verifyJWT = asyncHandler(async (req: Request, _, next: NextFunction) => {
   try {
-    const token: string =
+    const token: string | undefined =
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace(Bearer, "");
     if (!token) {
       throw new ApiError(401, "Missing or invalid access token");
     }
 
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!);
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECURE!);
     if (!decodedToken) {
       throw new ApiError(401, "Invalid access token");
     }
@@ -35,4 +36,19 @@ const verifyJWT = asyncHandler(async (req: Request, _, next: NextFunction) => {
   }
 });
 
-export { verifyJWT };
+const verifyPermission = (role: string[] = []) => {
+  console.log("role1:", role);
+  asyncHandler(async (req: Request, _, next: NextFunction) => {
+    if (!req.user?._id) {
+      throw new ApiError(401, "Unauthorized request");
+    }
+    if (role.includes(req.user?.role)) {
+      console.log("Includes:", role.includes(req.user?.role));
+      next();
+    } else {
+      throw new ApiError(403, "You are not authorized to perform this action");
+    }
+  });
+};
+
+export { verifyJWT, verifyPermission };
