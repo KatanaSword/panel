96d101d87a8881b22219a84ca51cf22c8e96a9f3
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-01-15 16:13:28 +0530

    hash password, check password, generate token

--- backend/src/utils/helpers.ts
+++ backend/src/utils/helpers.ts
@@ -1,18 +1,69 @@
-import { pgEnum } from "drizzle-orm/pg-core"; 
-import * as t from "drizzle-orm/pg-core"
-import crypto from "crypto"
+import { pgEnum } from "drizzle-orm/pg-core";
+import * as t from "drizzle-orm/pg-core";
+import crypto from "crypto";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { users } from "../drizzle/user.schema";
+import { USER_TEMPORARY_TOKEN_EXPIRY } from "../constants";
 
-export const availableUserRolesEnum = pgEnum("role", ["USER", "SUBSCRIBER", "ADMIN"])
-
-export const availablePaymentMethodEnum = pgEnum("payment_method", ["UNKNOWN", "RAZORPAY"])
+export const availablePaymentMethodEnum = pgEnum("payment_method", [
+  "UNKNOWN",
+  "RAZORPAY",
+]);
 
 export const timestamp = {
-    updated_at: t.timestamp("updated_at"),
-    created_at: t.timestamp("created_at").defaultNow().notNull(),
-    deleted_at: t.timestamp("deleted_at"),
+  updated_at: t.timestamp("updated_at"),
+  created_at: t.timestamp("created_at").defaultNow().notNull(),
+  deleted_at: t.timestamp("deleted_at"),
+};
+
+// To hash a password
+export async function hashPassword(password: string): Promise<string> {
+  return await bcrypt.hash(password, 10);
 }
 
-export const generateUniqueString = (length: number = 12): string => {
-    const uniqueString = crypto.randomBytes(length).toString("hex")
-    return uniqueString
+// To check a password
+export async function isPasswordValid(
+  password: string,
+  hashPassword: string
+): Promise<boolean> {
+  return await bcrypt.compare(password, hashPassword);
+}
+
+// Generate Token
+export function generateAccessToken() {
+  return jwt.sign(
+    {
+      id: users.id,
+      username: users.username,
+      email: users.email,
+    },
+    process.env.ACCESS_TOKEN_SECURE!,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRE }
+  ) as string;
+}
+
+export function generateRefreshToken() {
+  return jwt.sign(
+    {
+      id: users.id,
+    },
+    process.env.REFRESH_TOKEN_SECURE!,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRE }
+  ) as string;
+}
+
+export function generateTemporaryToken(): {
+  unHashedToken: string;
+  hashedToken: string;
+  tokenExpiry: number;
+} {
+  const unHashedToken = crypto.randomBytes(20).toString("hex");
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(unHashedToken)
+    .digest("hex");
+  const tokenExpiry = Date.now() + USER_TEMPORARY_TOKEN_EXPIRY;
+
+  return { unHashedToken, hashedToken, tokenExpiry };
 }
