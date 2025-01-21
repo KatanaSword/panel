8d01705bd3f0afe4494bd8f096bee2a18f2b89be
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-01-21 12:00:49 +0530

    added schema in db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,3 @@
-import connectDB from "./db/index.ts";
 import dotenv from "dotenv";
 import { app } from "./app.ts";
 
@@ -6,15 +5,15 @@ dotenv.config({
   path: "./.env",
 });
 
-connectDB()
-  .then(() => {
+(async function(): Promise<void> {
+  try {
     app.on("Error", (error) => {
       console.log("Error:", error);
     });
     app.listen(process.env.PORT, () => {
       console.log(`⚙️ Server is running at port: ${process.env.PORT}`);
     });
-  })
-  .catch((error) => {
+  } catch(error) {
     console.log("PostgreSQL connection failed!!!", error);
-  });
+  };
+})()
