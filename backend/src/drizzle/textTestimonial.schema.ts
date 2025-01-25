2ec11e9fc062ca267917110d8e833983caec9c2f
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-01-25 08:59:14 +0530

    refactor code

--- backend/src/drizzle/textTestimonial.schema.ts
+++ backend/src/drizzle/textTestimonial.schema.ts
@@ -1,33 +1,47 @@
 import { pgTable as table } from "drizzle-orm/pg-core";
-import * as t from 'drizzle-orm/pg-core'
+import * as t from "drizzle-orm/pg-core";
 import { users } from "./user.schema";
 import { timestamp } from "../utils/helpers";
 import { relations } from "drizzle-orm";
 
-export const textTestimonials = table("text_testimonials", {
-    id: t.uuid("id").primaryKey().defaultRandom(),
-    name: t.varchar("name", {length: 20}).notNull(),
-    company: t.varchar("company", {length: 20}),
+export const textTestimonials = table(
+  "text_testimonials",
+  {
+    id: t.uuid("id").defaultRandom().primaryKey(),
+    fullName: t.varchar("full_name", { length: 50 }).notNull(),
+    company: t.varchar("company", { length: 50 }),
+    email: t.varchar("email", { length: 250 }).notNull(),
     avatar: t.varchar("avatar").default(""),
-    review: t.varchar("review", {length:100}).notNull(),
+    testimonialTitle: t.varchar("testimonial_title", { length: 100 }).notNull(),
+    testimonial: t.varchar("testimonial", { length: 250 }).notNull(),
     socialLink: t.varchar("social_link"),
-    username: t.varchar("username"),
-    video: t.varchar("video"),
-    ownerId: t.uuid("owner_id").references(() => users.id).notNull(),
-    ...timestamp
-}, (table) => {
-    return [{
-        nameIndex: t.index("name_idx").on(table.name)
-    }]
-})
+    role: t.varchar("role"),
+    ownerId: t
+      .uuid("owner_id")
+      .references(() => users.id)
+      .notNull(),
+    ...timestamp,
+  },
+  (table) => {
+    return [
+      {
+        fullNameIndex: t.index("full_name_idx").on(table.fullName),
+        emailIndex: t.index("email_idx").on(table.email),
+      },
+    ];
+  }
+);
 
 // Relation
 
-export const textTestimonialTableRelation = relations(textTestimonials, ({ one }) => {
+export const textTestimonialTableRelation = relations(
+  textTestimonials,
+  ({ one }) => {
     return {
-        user: one(users, {
-            fields: [textTestimonials.ownerId],
-            references: [users.id]
-        })
-    }
-})
+      user: one(users, {
+        fields: [textTestimonials.ownerId],
+        references: [users.id],
+      }),
+    };
+  }
+);
