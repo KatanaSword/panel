f46ff5d3da53c85cfd91b074ea873286ce28e812
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-02-01 16:15:50 +0530

    Add multer setup for image and video uploads

--- backend/src/middlewares/multer.middleware.ts
+++ backend/src/middlewares/multer.middleware.ts
@@ -1,6 +1,6 @@
 import multer from "multer";
 
-const storage = multer.diskStorage({
+const imageStorage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "public/images");
   },
@@ -9,4 +9,39 @@ const storage = multer.diskStorage({
   },
 });
 
-export const upload = multer({ storage });
+const videoStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/videos");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+export const imageUpload = multer({
+  storage: imageStorage,
+  limits: {
+    fileSize: 1000000, // 1000000 Bytes = 1 MB
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(png|jpg)$/)) {
+      // upload only png and jpg format
+      return cb(new Error("Please upload a image"));
+    }
+    cb(null, true);
+  },
+});
+
+export const videoUpload = multer({
+  storage: videoStorage,
+  limits: {
+    fileSize: 10000000, // 10000000 Bytes = 10 MB
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(mp4|MPEG-4|mkv)$/)) {
+      // upload only mp4 and mkv format
+      return cb(new Error("Please upload a video"));
+    }
+    cb(null, true);
+  },
+});
