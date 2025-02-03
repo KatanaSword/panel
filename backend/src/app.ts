7247e4b77f35e092cbe90521350bcfbeefcb82af
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-02-03 17:44:36 +0530

    declare company roles routers

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,10 +18,12 @@ app.use(cookieParser());
 
 // Import router
 import userRouter from "./routers/user.router.ts";
+import companyRoleRouter from "./routers/companyRole.router.ts";
 import textTestimonialRouter from "./routers/textTestimonial.router.ts";
 
 // Declear router
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/company_roles", companyRoleRouter);
 app.use("/api/v1/text_testimonials", textTestimonialRouter);
 
 export { app };
