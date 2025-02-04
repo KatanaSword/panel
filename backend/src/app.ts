12f6bc115234e3da077d912a9429f884c66c9193
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-02-04 15:28:00 +0530

    declare video testimonial routers

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,14 +16,16 @@ app.use(express.urlencoded({ limit: "18kb", extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-// Import router
+// Import routers
 import userRouter from "./routers/user.router.ts";
 import companyRoleRouter from "./routers/companyRole.router.ts";
 import textTestimonialRouter from "./routers/textTestimonial.router.ts";
+import videoTestimonialRouter from "./routers/videoTestimonial.router.ts";
 
-// Declear router
+// Declare routers
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/company_roles", companyRoleRouter);
 app.use("/api/v1/text_testimonials", textTestimonialRouter);
+app.use("/api/v1/video_testimonials", videoTestimonialRouter);
 
 export { app };
