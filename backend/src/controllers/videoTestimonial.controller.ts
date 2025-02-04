34ba91acf423b4b115b0755af0868f942343a5fd
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-02-04 12:15:24 +0530

    feat: fetch video testimonial

--- backend/src/controllers/videoTestimonial.controller.ts
+++ backend/src/controllers/videoTestimonial.controller.ts
@@ -3,18 +3,24 @@ import { Request, Response } from "express";
 import {
   avatarSchema,
   createVideoTestimonialSchema,
-  testimonialSchema,
+  videoSchema,
+  videoTestimonialIdSchema,
 } from "../validations/schemas/videoTestimonial.schema";
 import { ApiError } from "../utils/ApiError";
 import db from "../db";
 import { videoTestimonials } from "../drizzle/videoTestimonial.schema";
 import { ApiResponse } from "../utils/ApiResponse";
+import * as x from "drizzle-orm";
+
+const getAllVideoTestimonials = asyncHandler(
+  async (req: Request, res: Response) => {}
+);
 
 const createVideoTestimonial = asyncHandler(
   async (req: Request, res: Response) => {
     const parserData = createVideoTestimonialSchema.safeParse(req.body);
     const imageLocalPath = avatarSchema.safeParse(req.file?.path);
-    const videoLocalPath = testimonialSchema.safeParse(req.file?.path);
+    const videoLocalPath = videoSchema.safeParse(req.file?.path);
     const errorMessage = parserData.error?.issues.map(
       (issue) => issue.message
     ) as string[] | undefined;
@@ -46,26 +52,13 @@ const createVideoTestimonial = asyncHandler(
         fullName: parserData.data.fullName,
         email: parserData.data.email,
         company: parserData.data.company,
-        role: parserData.data.role,
+        companyRole: parserData.data.companyRole,
         socialLink: parserData.data.socialLink,
         avatar: uploadImage,
-        testimonial: uploadVideo,
+        video: uploadVideo,
         ownerId: req.user.id,
       })
-      .returning({
-        id: videoTestimonials.id,
-        fullName: videoTestimonials.fullName,
-        email: videoTestimonials.email,
-        company: videoTestimonials.company,
-        role: videoTestimonials.role,
-        socialLink: videoTestimonials.socialLink,
-        avatar: videoTestimonials.avatar,
-        testimonial: videoTestimonials.testimonial,
-        ownerId: videoTestimonials.ownerId,
-        created_at: videoTestimonials.created_at,
-        updated_at: videoTestimonials.updated_at,
-        deleted_at: videoTestimonials.deleted_at,
-      });
+      .returning();
     if (!createVideoTestimonial) {
       throw new ApiError(
         500,
@@ -85,4 +78,55 @@ const createVideoTestimonial = asyncHandler(
   }
 );
 
-export { createVideoTestimonial };
+const getVideoTestimonialById = asyncHandler(
+  async (req: Request, res: Response) => {
+    const parserId = videoTestimonialIdSchema.safeParse(req.params);
+    if (!parserId.success) {
+      throw new ApiError(400, "Video testimonial id is missing or invalid");
+    }
+
+    const videoTestimonial = await db.query.videoTestimonials.findFirst({
+      where: parserId.data.videoTestimonialId
+        ? x.eq(videoTestimonials.id, parserId.data.videoTestimonialId)
+        : undefined,
+    });
+    if (!videoTestimonial) {
+      throw new ApiError(404, "Video testimonial not found");
+    }
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, videoTestimonial, "Fetch video testimonial"));
+  }
+);
+
+const getVideoTestimonialByCampanyRole = asyncHandler(
+  async (req: Request, res: Response) => {}
+);
+
+const updateVideoTestimonial = asyncHandler(
+  async (req: Request, res: Response) => {}
+);
+
+const updateVideoTestimonialAvatar = asyncHandler(
+  async (req: Request, res: Response) => {}
+);
+
+const updateVideoTestimonialVideo = asyncHandler(
+  async (req: Request, res: Response) => {}
+);
+
+const deleteVideoTestimonial = asyncHandler(
+  async (req: Request, res: Response) => {}
+);
+
+export {
+  createVideoTestimonial,
+  getVideoTestimonialById,
+  updateVideoTestimonial,
+  updateVideoTestimonialAvatar,
+  updateVideoTestimonialVideo,
+  getVideoTestimonialByCampanyRole,
+  deleteVideoTestimonial,
+  getAllVideoTestimonials,
+};
