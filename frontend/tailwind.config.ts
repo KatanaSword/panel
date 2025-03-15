c8ca049a9b069e562c067792239e9708d0230e35
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-03-15 11:46:17 +0530

    tailwind config file

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,141 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
+import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      md: {
+        max: "1050px",
+      },
+      sm: {
+        max: "550px",
+      },
+    },
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          1: "hsl(var(--chart-1))",
+          2: "hsl(var(--chart-2))",
+          3: "hsl(var(--chart-3))",
+          4: "hsl(var(--chart-4))",
+          5: "hsl(var(--chart-5))",
+        },
+        black: {
+          B600: "#1A191B",
+          B700: "#1F1F1F",
+          B800: "#0D0D0D",
+          B900: "#000000",
+        },
+        gray: {
+          G400: "#ACACAC",
+        },
+        orange: {
+          O300: "#FF873C",
+          O400: "#FF7926",
+          O600: "#EE630C",
+        },
+        walnut_brown: {
+          W700: "#3D2F26",
+        },
+      },
+      fontFamily: {},
+      boxShadow: {
+        video_box_shadow:
+          "rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 56px, rgba(17, 17, 26, 0.1) 0px 24px 80px",
+      },
+      backgroundImage: {
+        gradient:
+          "linear-gradient(90deg, rgba(112,68,34,1) 0%, rgba(31,24,21,1) 100%)",
+        hero_tagline_gradient:
+          "linear-gradient(90deg, rgba(243,112,38,1) 0%, rgba(250,222,205,1) 100%)",
+        h2_gradient:
+          "linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(149,149,149,1) 100%)",
+        pro_pricing_gradient:
+          "linear-gradient(140deg, rgba(255,148,82,1) 0%, rgba(255,255,255,1) 100%)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+    },
+  },
+  plugins: ["tailwindcss-animate", addVariablesForColors],
+};
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
